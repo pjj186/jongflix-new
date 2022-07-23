0f5dca774b984e8b28f4bfe5dfcb4d00af91e8e1
Jong jae Park <pjj186@naver.com>
2022-07-24 01:24:12 +0900

    Home

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "0c6f3c84d4564ed4f2afcb7aa3744089";
 const BASE_PATH = "https://api.themoviedb.org/3/";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  data: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
