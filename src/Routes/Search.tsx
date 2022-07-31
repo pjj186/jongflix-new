1f862752734e54650840c0f77816c9a4ad515302
Jong jae Park <pjj186@naver.com>
2022-07-31 16:57:17 +0900

    Update Search.tsx

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 const Search = () => {
   const location = useLocation();
   const keyword = new URLSearchParams(location.search).get("keyword");
-  return null;
+  return <div></div>;
 };
 
 export default Search;
